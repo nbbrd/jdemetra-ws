45be998494653a21a30ce283860749baebc31b8f
romaintailhurat <romain.tailhurat@gmail.com>
2017-01-04 15:23:46 +0100

    Adding a CORS filter class compatible with the Jersey resource config class in the standalone server

---
+++ ws-commons/src/main/java/ec/nbb/ws/filters/CORSFilter.java
@@ -0,0 +1,23 @@
+package ec.nbb.ws.filters;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+/**
+ * CORS Filter 
+ * */
+public class CORSFilter implements ContainerResponseFilter {
+
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");        
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");            
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    }
+
+}
